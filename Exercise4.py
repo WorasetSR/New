502aa0cb29da8c5dd82493adec3b691fb7547216
WorasetSR <woraset.sumrit@gmail.com>
2025-06-29 21:18:18 +0700

    Create Exercise4.py

---
+++ Exercise4.py
@@ -0,0 +1,21 @@
+"""
+Foundation English = FE
+General Business = GB
+Introduction to Computer Systems = ICS
+Computer Programming = CP
+"""
+print("-----------------------------------------------------------")
+FE = float(input("Your Foundation English score : "))
+GB = float(input("Your General Business score : "))
+ICS = float(input("Your Introduction score : "))
+CP = float(input("Your Computer System score : "))
+print("-----------------------------------------------------------")
+print("Your scores")
+print("Foundation English score is:", FE)
+print("General Business score is:", GB)
+print("Introduction score is:", ICS)
+print("Computer System score is:", CP)
+print("--------------------------------------------------------------")
+print("Summary :", (FE+GB+ICS+CP)/4)
+_kpkpk = 10
+print(_kpkpk+1)
