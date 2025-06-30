1f723321de18f6760bd2fb15e1ad0d64212c4df6
WorasetSR <woraset.sumrit@gmail.com>
2025-06-30 18:40:12 +0700

    Update Exercise 8.py

--- Lecture/Exercise 8.py
+++ Lecture/Exercise 8.py
@@ -5,22 +5,21 @@ if un == 'Poomza' and pw == '007x' :
 - What do you want?
 1. Superman Underwear
 2. Superman Socks
-3. Eyes Superman
-4. Superpower Superman''')
+3. Superpower''')
     c = input("Enter your choice : ")
     if c == '1' :
         print("Superman Underwear 99 Bath")
         p = float(input("Price : "))
-        print("Summary", p*99, 'Bath')
+        print("Total cost", p*99, 'Bath')
 
     if c == '2' :
         print("Superman Socks 79 Bath")
         p = float(input("Price : "))
-        print("Summary", p*79, 'Bath')
+        print("Total cost", p*79, 'Bath')
     if c == '3' :
-        print("Superpower Superman 99999 Bath")
+        print("Superpower 99999 Bath")
         p = float(input("Price : "))
-        print("Summary", p * 99999, 'Bath')
+        print("Total cost", p * 99999, 'Bath')
 else :
     print("Failed")
 
