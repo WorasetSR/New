b6f0c168d93568554028e88f175b37a44b49ecf0
WorasetSR <woraset.sumrit@gmail.com>
2025-07-03 15:22:46 +0700

    d

--- Exercise Devlab4/7.py
+++ Exercise Devlab4/7.py
@@ -1,22 +1,11 @@
 import math
 
 x = int(input(''))
-y =int(x//7)
-w = int(math.ceil(x/10))
-u = int(x//70)
-lis = {7}
-t = ''
-if x// 7 == 0 :
-    print('no seven')
+y = []
+for i in range(1, x+1) :
+    if i % 7 == 0 or "7" in str(i):
+        y.append(str(i))
+if y :
+    print(','.join(y))
 else:
-    for a in range(y) :
-        t = 7*(a+1)
-        lis.add(t)
-    for a in range(w) :
-        t = 7+a*10
-        lis.add(t)
-    if u > 0 :
-        for a in range(u-1) :
-            t = 7*(a+1)*10
-            lis.add(t)
-    print(','.join(str(x) for x in sorted(lis)))
+    print('no seven')
